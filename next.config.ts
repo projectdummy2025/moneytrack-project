3f5a8a9a7ea0ea240ea51edb43534f960093fd1f
Ahmad Dedad <projectdummy2025@gmail.com>
2026-04-05 09:57:07 +0700

    update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import os from "os";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: Object.values(os.networkInterfaces()).flat().map(i => i?.address || "localhost:3000").filter(Boolean),
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS,PUT,DELETE,PATCH" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
